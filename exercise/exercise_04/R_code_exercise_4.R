33736c00c12abb417c5826c59f3ca6ec70d15fee
jens.klenke <57100601+jens-klenke@users.noreply.github.com>
2022-12-03 00:33:49 +0100

    Exercise update
    - Exercise sheet 4 updates
    - Exercise sheet 5
    - R-Code update

--- exercise/exercise_04/R_code_exercise_4.R
+++ exercise/exercise_04/R_code_exercise_4.R
@@ -12,8 +12,6 @@ which.max(table(iris$Sepal.Width))
 # c)
 mean(iris$Sepal.Width)
 
-
-
 # Aufgabe 2
 
 # c) 
@@ -21,3 +19,14 @@ summe <- 130 * 3200 + (1000 + 1800 + 5000 + 6072)
 summe/134
 
 (130 * 3200 + (1000 + 1800 + 5000 + 6072))/134
+
+# Aufgabe 4
+gas_consumption <- c(386.4, 312, 300, 194.4, 84, 52.8, 
+                     40.8, 38.4, 124.8, 201.6, 292.8, 372)
+# a)
+mean_gas_consumption <- mean(gas_consumption)
+mean_gas_consumption
+
+# b) 
+kWh <- 9.3 * 0.9103 * mean_gas_consumption
+kWh
