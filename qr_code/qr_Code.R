0937be68f943da81ca8f8f1d037e9aff9b817a65
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2023-03-11 15:36:00 +0100

    QR-Code changed

--- qr_code/qr_Code.R
+++ qr_code/qr_Code.R
@@ -10,3 +10,17 @@ plot(code)
 generate_svg(code, filename = here::here('qr_code/qr_code.svg'), 
               size = 600)
 
+
+
+######
+
+code <- qr_code('https://www.km-bw.de/site/pbs-bw-new/get/documents/KULTUS.Dachmandant/KULTUS/Dienststellen/lis-in-bw/Schulsport/WSB/Bewegungspausen_n.pdf', ecl = 'H')
+code_1 <- qr_code('https://www.dropbox.com/s/be0ygpldyp82m2i/d2_Test.pdf?dl=0', ecl = 'H')
+print(code)
+plot(code_1)
+
+# svg 
+generate_svg(code, filename = here::here('qr_code/qr_code.svg'), 
+             size = 600, foreground = "#004C93")
+generate_svg(code_1, filename = here::here('qr_code/qr_code.svg'), 
+             size = 600, foreground = "#004C93")
